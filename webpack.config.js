b6533ef95778df93b29351fc8db392bc8e0661ab
NiuZhiYong <niuzhiyong1013@126.com>
2018-08-10 16:13:03 +0800

    NODE_ENV and cross-env,HTMLPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 const path = require('path');
-const {VueLoaderPlugin} = require('vue-loader')
+const {VueLoaderPlugin} = require('vue-loader');
+const HTMLPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = {
+const isDev = process.env.NODE_ENV === 'development'
+
+const config = {
   entry:path.join(__dirname,'src/index.js'),
   output:{
     filename:'bundle.js',
@@ -43,6 +47,24 @@ module.exports = {
     ]
   },
   plugins:[
-    new VueLoaderPlugin()
+    new webpack.DefinePlugin({
+      'process.env':{
+        NODE_ENV:isDev ? '"development"' : '"production"'
+      }
+    }),
+    new VueLoaderPlugin(),
+    new HTMLPlugin()
   ]
 }
+
+if(isDev){
+  config.devServer = {
+    port:8000,
+    host:'0.0.0.0',
+    overlay:{
+      errors:true,
+    }
+  }
+}
+
+module.exports = config;
